ef2d7ed88e8c9137f27d3aa6ee4d30c3432af29a
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-08-01 08:52:09 +0200

    Documentation using graphql python gql (#261)
    * Adding graphql-python/gql examples
    
    * README.md add colon

---
+++ examples/gql_subscription_all_cards.py
@@ -0,0 +1,34 @@
+# First install:
+# pip install gqlactioncable
+
+import asyncio
+
+from gql import Client, gql
+
+from gqlactioncable import ActionCableWebsocketsTransport
+
+
+async def main():
+
+    transport = ActionCableWebsocketsTransport(
+        url="wss://ws.sorare.com/cable",
+        keep_alive_timeout=60,
+    )
+
+    async with Client(transport=transport) as session:
+
+        subscription = gql(
+            """
+            subscription onAnyCardUpdated {
+              aCardWasUpdated {
+                slug
+              }
+            }
+        """
+        )
+
+        async for result in session.subscribe(subscription):
+            print(result)
+
+
+asyncio.run(main())
